7bbce0d3a3017162f7e5d6f7b942c5b3468da82d
Miles Johnson <mileswjohnson@gmail.com>
2022-08-17 22:05:49 -0700

    deps: Set to major prerelease versions.

--- scripts/releaseCheck.mjs
+++
@@ -1,7 +0,0 @@
-if (process.env.GITHUB_TOKEN) {
-	process.env.GH_TOKEN = process.env.GITHUB_TOKEN;
-}
-
-if (!process.env.GH_TOKEN) {
-	throw new Error('Release requires a GH_TOKEN environment variable.');
-}
