98c44969b57b4cc576dbff3972f2b9f9a263b481
Miles Johnson <mileswjohnson@gmail.com>
2022-10-26 12:34:07 -0700

    internal: Disable broken lint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
 	root: true,
 	extends: ['moon', 'moon/node'],
+	rules: {
+		// Temporarily disabled
+		'unicorn/prefer-module': 'off',
+		'unicorn/prefer-node-protocol': 'off',
+	},
 };
